ac12eb7546fcd30a313faa951832848f1abcf3e2
Allan Lasser <lasser.allan@gmail.com>
2014-04-08 19:44:38 -0400

    Reads and displays frames from folder of images
    This is the most basic foundation for performing operations on video frames.

--- VS2012_Solution/Car_Tracker/Car_Tracker.cpp
+++ VS2012_Solution/Car_Tracker/Car_Tracker.cpp
@@ -1,48 +1,47 @@
-// CS585 Lab 1, Part 0
-// Hello, CS585!
-//
-// This program reads an image from a file, then displays the image.
-// The program waits for the user to press a key to end the program, 
-// prints the key that was pressed, then exits.
-//
-// Copyright 2014 Diane H. Theriault
-//
-
 #include "stdafx.h"
 #include <iostream>
+#include <ctype.h>
+#include <string>
+#include <vector>
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
 
 using namespace std;
 using namespace cv;
 
-// the main() function is the entry point for all C++ programs
-int main(int argc, char* argv[])
-{
-    //cout is the command to print things to the command line in C++
-    //endl inserts an end of line character / line break
-    cout<<"Hello CS585!"<<endl;
+int main(int argc, char* argv[]) {
+	int frame_counter = 0;
+	int frame_total = 114;
+	String frame_folder = "./data/";
+	String frame_filetype = ".png";
+	while (frame_counter < frame_total) {
+		// Read current image as video frame
+		// Every image files is 10 digits + filetype
+		// Currently supports up to 999,999 frames
+		String filename = frame_folder;
+		if (frame_counter < 10) {
+			filename += "000000000";
+		} else if (frame_counter < 100) {
+			filename += "00000000";
+		} else if (frame_counter < 1000) {
+			filename += "0000000";
+		} else if (frame_counter < 10000) {
+			filename += "000000";
+		} else {
+			filename += "00000";
+		}
+		filename += to_string(frame_counter) + frame_filetype;
 
-    //Mat is the OpenCV container for images and matrices
-    Mat image;
+		Mat frame;
+		frame = imread(filename, 0);
+		
+		/* Frame operations go here! */
 
-    //imread is the OpenCV function for reading images from a file
-    image = imread("HelloCS585.jpg"); 
-
-    //namedWindow is the OpenCV function for creating a new window
-    namedWindow( "Image View", 1 );
-
-    //imshow is the OpenCV command for display an image in a window. 
-    //The arguments are the name of the window, used with the namedWindow() function, 
-    //and the image data, in a Mat structure
-    imshow("Image View", image);
-    char key = waitKey(0);
-
-    //Using cout, different types of output can be chained together using the << operator
-    cout<<"You pressed "<<key<<"."<<endl;
-
-    //in C++, main returns an integer, which is usually 0 by convention
-    return 0;
+		imshow("Frame", frame);
+		if (waitKey(10) == 'q')
+			break;
+		frame_counter++;
+	}
 }
-
