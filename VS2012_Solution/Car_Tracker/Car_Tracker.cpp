282680533e39ab65037e5e6d90bc0b499625e31f
James Racine <griffonlord@gmail.com>
2014-04-25 22:24:41 -0400

    Added mostly complete speed calculations code
    It still is incomplete due to lack of optical flow, and a couple small
    fixes once I have that.

--- VS2012_Solution/Car_Tracker/Car_Tracker.cpp
+++ VS2012_Solution/Car_Tracker/Car_Tracker.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include <iostream>
+#include <fstream>
 #include <ctype.h>
 #include <string>
 #include <vector>
@@ -16,6 +17,13 @@ Rect getROI (Point& point, Size patchSize, Size imageSize, int searchRadius);
 Rect trackTemplate (Point& point, Mat& patch, Mat& image, int searchRadius);
 vector<Mat> getPatches (Mat& frame, vector<Rect> cars);
 void drawDetections (Mat& frame, vector<Rect> cars);
+
+/*camera speeed stuff*/
+vector<float> getCarSpeeds(vector<Rect> cars, int &counter, int frame);
+float getCameraSpeed(int &counter, int frame);
+vector<float> getTimeStampsGPS();
+vector<float> getTimeStampsVideo();
+/*end camera speed stuff*/
 
 int main(int argc, char* argv[]) {
 	int frame_counter = 0;
@@ -140,4 +148,111 @@ void drawDetections (Mat& frame, vector<Rect> cars) {
 		Point bottomRight = Point(cars[i].x+cars[i].width, cars[i].y+cars[i].height);
 		rectangle(frame, topLeft, bottomRight, Scalar(255, 255, 255), 2, 8, 0);
 	}
+}
+
+vector<float> getCarSpeeds(vector<Rect> cars, int counter, int frame){
+	float ratio = getCameraSpeed(counter,frame);		 
+
+	Mat opticalFlow; //dummy, same as below
+
+	vector<float> speedLabels;
+	for( size_t i = 0; i < cars.size(); i++ ){
+		speedLabels.at(i) = mean(opticalFlow(cars.at(i)))[0] * ratio;
+	}
+}
+
+
+float getGPSVelocity(int counter){
+	vector<float> GPS;//read_data_for_frame
+	String dataFile = "./data/oxts/data/";
+	if (counter < 10) {
+			dataFile += "000000000";
+		} else if (counter < 100) {
+			dataFile += "00000000";
+		} else if (counter < 1000) {
+			dataFile += "0000000";
+		} else if (counter < 10000) {
+			dataFile += "000000";
+		} else {
+			dataFile += "00000";
+		}
+	dataFile += to_string(counter) + ".txt";
+	char temp[256];
+	std::ifstream ifs (dataFile,std::ifstream::in);
+	int size = 0;
+	while(ifs.good() && (size < 9)){
+		ifs.get(temp,10,' ');
+		GPS.push_back(atof(temp));
+		size ++;
+	}
+	return length(GPS.at(6),GPS.at(7));
+}
+
+float getCameraSpeed(int &counter, int frame){
+	float cameraVelocity = 0.0;
+
+	vector<float> GPSTime = getTimeStampsGPS();
+	vector<float> FrameTime = getTimeStampsVideo();
+
+	while(FrameTime.at(frame) > GPSTime.at(counter)){
+		cameraVelocity += getGPSVelocity(counter);
+		counter++;
+	}
+
+	Rect frontOfCar = Rect(); //dummy  //(shoudl be roughly the front of the car, say, middle fifth of the x, lowest quarter of the y
+
+	Mat opFlow;//assumeing you guys are covereing that part, either I'll pass it through as a pointer or we store it globablly
+	Mat focRegion = opFlow(frontOfCar);
+
+	double pixelVelocity = mean(focRegion)[0]; //this is how you get the average, though th e0 means for only the first channel
+	double realToImage = pixelVelocity/cameraVelocity; //this works as we are treated the time between frames as 1
+
+	return realToImage;
+}
+
+/*
+Current problem is how to best manage the difference in data vs. frames. aka more gps data than frames
+math is easy, just take all relevant readings and average them. simple
+but.
+actually no, it is simple. just keep track of the number in that main loop and pass it. easy.
+*/
+
+float length(float x, float y){
+	return sqrtf(x*x+y*y);
+}
+
+vector<float> getTimeStampsGPS(){
+	String timeStampFile = "./data/oxts/timestamps.txt";
+	std::ifstream ifs (timeStampFile, std::ifstream::in);
+	vector<float> timeStamps;
+	char temp[256];
+	String tempB;
+	while(ifs.good()){
+		ifs.getline(temp,256);
+		tempB = temp;
+		if(!tempB.empty()){
+			tempB = tempB.substr(17);
+			timeStamps.push_back(stof(tempB));
+		}
+	}
+
+	return timeStamps;
+}
+
+vector<float> getTimeStampsVideo(){
+	String timeStampFile = "./data/video/timestamps.txt";
+	std::ifstream ifs (timeStampFile, std::ifstream::in);
+	vector<float> timeStamps;
+	char temp[256];
+	String tempB;
+	while(ifs.good()){
+		ifs.getline(temp,256);
+		tempB = temp;
+		if(!tempB.empty()){
+			tempB = tempB.substr(17);
+			timeStamps.push_back(stof(tempB));
+		}
+	}
+
+	return timeStamps;
 }
